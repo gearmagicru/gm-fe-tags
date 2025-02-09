e3258fee526ddc1c5ae9c9e5157849ec1b8ee9b2
Anton Tivonenko <anton.tivonenko@gmail.com>
2025-02-09 19:28:58 +0300

    Файлы локализации

---
+++ lang/text-en_GB.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Этот файл является частью модуля веб-приложения GearMagic.
+ * 
+ * Пакет английской (британской) локализации.
+ * 
+ * @link https://gearmagic.ru
+ * @copyright Copyright (c) 2015 Веб-студия GearMagic
+ * @license https://gearmagic.ru/license/
+ */
+
+return [
+    '{name}'        => 'Search by the specified tag',
+    '{description}' => 'Output objects associated with the specified tag',
+    '{permissions}' => [],
+];
---
+++ lang/text-ru_RU.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Этот файл является частью модуля веб-приложения GearMagic.
+ * 
+ * Пакет русской локализации.
+ * 
+ * @link https://gearmagic.ru
+ * @copyright Copyright (c) 2015 Веб-студия GearMagic
+ * @license https://gearmagic.ru/license/
+ */
+
+return [
+    '{name}'        => 'Поиск объектов по указанной метки',
+    '{description}' => 'Вывод объектов (содержимого) связанных с указанной меткой',
+    '{permissions}' => [],
+];
